2f165ffce0f87e7e3294b08336654efae0fc306e
root <elmehdibouslam@gmail.com>
2025-01-27 18:17:12 -0800

    final commit

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,26 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import sys
 
-if (len(sys.argv) != 2):
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
 
 n = sys.argv[1]
 
-if (not n.isdigit()):
+if not n.isdigit():
     print("N must be a number")
     exit(1)
 
 n = int(n)
-if (n < 4):
+if n < 4:
     print("N must be at least 4")
     exit(1)
 
 
 def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-
+    """Find possible positions"""
     if i < n:
         for j in range(n):
             if j not in a and i + j not in b and i - j not in c:
@@ -30,16 +29,9 @@ def queens(n, i=0, a=[], b=[], c=[]):
 
 
 def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
+    """Solve the N-queens problem"""
+    for solution in queens(n):
+        print([[i, solution[i]] for i in range(n)])
 
 
 solve(n)
